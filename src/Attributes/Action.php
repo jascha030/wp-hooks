3d57be03be06c5595c91dd3b3ac28a36b33f27af
Jascha030 <contact@jaschavanaalst.nl>
2022-08-16 22:52:44 +0200

    ✨ Implement Action and Filter Attributes.

---
+++ src/Attributes/Action.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Jascha030\Hooks\Attributes;
+
+use Attribute;
+use Jascha030\Hooks\AssertClosureTrait;
+
+#[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
+final class Action extends HookAttributeAbstract
+{
+    use AssertClosureTrait;
+
+    public function add(callable|\Closure $callable, int $acceptedArgs): void
+    {
+        \Jascha030\Hooks\Action::add(
+            $this->getTag(),
+            $this->assertClosure($callable),
+            $this->getPrio(),
+            $acceptedArgs
+        );
+    }
+}
